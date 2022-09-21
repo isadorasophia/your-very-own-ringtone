995842f3513b3d92a58c6d4d305ac69584d1bd6f
Isadora Rodopoulos <isgarcia@microsoft.com>
2022-09-21 13:45:34 -0700

    VSIX project initialize.

---
+++ YourVeryOwnRingtone/OptionsPage.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace YourVeryOwnRingtone
+{
+    public sealed class OptionsPage : DialogPage
+    {
+        private string _configurationFile = string.Empty;
+
+        private Func<string, Task>? _onUpdate;
+
+        public async Task InitializeAsync(SoundManager manager)
+        {
+            _onUpdate = manager.RefreshSoundsAsync;
+            await RefreshAsync();
+        }
+
+        public async Task RefreshAsync()
+        {
+            if (_onUpdate is null)
+            {
+                return;
+            }
+
+            await _onUpdate.Invoke(_configurationFile);
+        }
+
+        [Category("Sounds Settings")]
+        [DisplayName("Configuration File")]
+        [Description("Path to your json specifying a list of .wav sounds.")]
+        public string ConfigurationFile
+        { 
+            get => _configurationFile;
+            set
+            {
+                _configurationFile = value;
+                _ = RefreshAsync();
+            }
+        }
+    }
+}
---
+++ YourVeryOwnRingtone/SoundManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System;
+
+#nullable enable
+
+namespace YourVeryOwnRingtone
+{
+    [Export(typeof(SoundManager))]
+    public sealed class SoundManager
+    {
+        private readonly string[] AvailableSounds = new string[] 
+            { "breakpoint", "step", "exception" };
+
+        public Dictionary<string, System.Media.SoundPlayer> _sounds = new();
+
+        private readonly object _lock = new();
+
+        /// <summary>
+        /// Refresh all the sounds currently available in the library.
+        /// </summary>
+        public async Task RefreshSoundsAsync(string configurationFile)
+        {
+            lock (_lock)
+            {
+                _sounds.Clear();
+            }
+
+            if (!File.Exists(configurationFile))
+            {
+                return;
+            }
+
+            string jsonText = await ReadAllTextAsync(configurationFile);
+
+            Dictionary<string, string> sounds;
+            try
+            {
+                sounds = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText)!;
+            }
+            catch
+            {
+                throw new ArgumentException("Unable to deserialize configuration file.");
+            }
+
+            lock (_lock)
+            {
+                foreach (string sound in AvailableSounds)
+                {
+                    if (sounds.TryGetValue(sound, out string path) && File.Exists(path))
+                    {
+                        System.Media.SoundPlayer player = new(path);
+                        player.LoadAsync();
+
+                        _sounds[sound] = player;
+                    }
+                }
+            }
+        }
+
+        private async Task<string> ReadAllTextAsync(string file)
+        {
+            using StreamReader reader = File.OpenText(file);
+
+            StringBuilder result = new();
+
+            while (await reader.ReadLineAsync() is string line)
+            {
+                result.Append(line);
+            }
+
+            return result.ToString();
+        }
+
+        public void PlaySound(string name)
+        {
+            if (_sounds.TryGetValue(name, out var sound))
+            {
+                sound.Play();
+            }
+        }
+    }
+}
